0e6062492828890fe623826a47bf57a9d57f72ca
anisha stha <stha.anisha16@gmail.com>
2024-08-26 00:00:20 +0545

    feat:components

--- casecobra-dev/src/app/page.tsx
+++ casecobra-dev/src/app/page.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3"></MaxWidthWrapper>
+      </section>
   
    </div>
   );
