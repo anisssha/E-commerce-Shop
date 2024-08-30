772a288776fc1e7e91b68f6eeed2e2b86c56e40f
anisha stha <stha.anisha16@gmail.com>
2024-08-30 20:30:00 +0545

    💄 update the UI and style files.

--- casecobra-dev/src/app/page.tsx
+++ casecobra-dev/src/app/page.tsx
@@ -10,9 +10,13 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <img src="/snake-1.png" />
+                <img src="/snake-1.png " className="w-full" />
 
               </div>
+              <h1 className="relative w-fit tracking-tighter text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl ">
+                Your Image on a <span className="bg-green-600 px-2 text-white rounded">Custom</span>
+                Phone Case
+              </h1>
             </div>
 
           </div>
